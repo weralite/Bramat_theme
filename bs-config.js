231e9a77dd4953c48278954729dec1a7474f484c
mazmann <sadi.lundberg@hotmail.com>
2024-10-02 09:45:36 +0200

    added sync for reloading browser when editing code

--- bs-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    proxy: "presswind.test",
-    files: ["**/*.php", "js/app.js", "css/app.css", "css/editor-style.css"],
-    notify: false,
-    open: false,
-    reloadDelay: 500
-};
