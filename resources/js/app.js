9b6aba3d942ee4f37b36f8c4978031292ddf41a6
mazmann <sadi.lundberg@hotmail.com>
2024-10-10 00:15:51 +0200

    hre

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,33 +7,33 @@ window.addEventListener('load', function () {
       });
 });
 
-function appendSpanToSecondaryMenuItems() {
-      // Find the ul element with the ID "menu-secondary-menu"
-      const secondaryMenu = document.getElementById('menu-secondary-menu');
+// function appendSpanToSecondaryMenuItems() {
+//       // Find the ul element with the ID "menu-secondary-menu"
+//       const secondaryMenu = document.getElementById('menu-secondary-menu');
   
-      // Check if the element exists
-      if (secondaryMenu) {
-          // Find all li elements within the ul
-          const menuItems = secondaryMenu.querySelectorAll(':scope > li');
+//       // Check if the element exists
+//       if (secondaryMenu) {
+//           // Find all li elements within the ul
+//           const menuItems = secondaryMenu.querySelectorAll(':scope > li');
   
-          // Iterate over each li element
-          menuItems.forEach((menuItem) => {
-              // Create a new span element
-              const newSpan = document.createElement('span');
+//           // Iterate over each li element
+//           menuItems.forEach((menuItem) => {
+//               // Create a new span element
+//               const newSpan = document.createElement('span');
   
-              // Optionally, you can add some content or attributes to the span
-              newSpan.textContent = '';
-              newSpan.className = 'absolute left-0 top-0 w-full h-0 transition-all duration-200 bg-gray-300 z-5 group-hover:h-full'; // Add a class if needed
+//               // Optionally, you can add some content or attributes to the span
+//               newSpan.textContent = '';
+//               newSpan.className = 'absolute left-0 top-0 w-full h-0 transition-all duration-200 bg-gray-300 z-5 group-hover:h-full'; // Add a class if needed
   
-              // Append the span to the li element
-              menuItem.appendChild(newSpan);
-          });
-      } else {
-          console.error('Element with ID "menu-secondary-menu" not found.');
-      }
-  }
+//               // Append the span to the li element
+//               menuItem.appendChild(newSpan);
+//           });
+//       } else {
+//           console.error('Element with ID "menu-secondary-menu" not found.');
+//       }
+//   }
   
-  window.addEventListener('load', appendSpanToSecondaryMenuItems);
+//   window.addEventListener('load', appendSpanToSecondaryMenuItems);
 
 
 //   function toggleSubmenuOnHover() {
