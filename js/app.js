31f11131541fb1745a1e31b4e3d9e15d08206935
mazmann <sadi.lundberg@hotmail.com>
2024-10-15 19:23:13 +0200

    removed consoel logs

--- js/app.js
+++ js/app.js
@@ -7,13 +7,10 @@
   \*****************************/
 /***/ (() => {
 
-console.log('app.js is loaded');
-
 // Navigation toggle
 window.addEventListener('load', function () {
   var main_navigation = document.querySelector('#primary-menu');
   document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
-    console.log('click');
     e.preventDefault();
     main_navigation.classList.toggle('hidden');
   });
