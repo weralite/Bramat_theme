ff54e8e232f4e74769d1ed94a76798348d404b2a
mazmann <sadi.lundberg@hotmail.com>
2024-10-22 23:30:40 +0200

    header file cleaned

--- header.php
+++ header.php
@@ -19,30 +19,30 @@
 		<header class="sticky top-0 z-10 min-w-full">
 
 				<!-- Main navigational menu for desktop view -->
-			<?php
-		wp_nav_menu( array(
-    		'theme_location'  => 'primary',
-			'container'       => 'nav',
-			'container_id'    => 'primary-menu',
-    		'container_class' => 'min-w-full flex justify-center', 
-   			'menu_class'      => 'hidden xl:flex xl:flex-row xl:justify-center', 
-   		    'li_class'        => 'primary-parent-li group', 
- 		    'a_class'         => 'ajax-link xl:text-2xl z-10 xl:tracking-wide xl:font-teko xl:w-full xl:font-light xl:uppercase xl:relative',
- 		    'submenu_class'   => 'hover-background-animation', 
-			'fallback_cb'     => false,
-			));?>
+
+<?php
+					wp_nav_menu( array(
+    						'theme_location'  => 'primary',
+							'container'       => 'nav',
+							'container_id'    => 'primary-menu',
+    						'container_class' => 'min-w-full flex justify-center', 
+   							'menu_class'      => 'hidden xl:flex xl:flex-row xl:justify-center', 
+   						    'li_class'        => 'primary-parent-li group', 
+ 						    'a_class'         => 'ajax-link xl:text-2xl z-10 xl:tracking-wide xl:font-teko xl:w-full xl:font-light xl:uppercase xl:relative',
+ 						    'submenu_class'   => 'hover-background-animation', 
+							'fallback_cb'     => false,
+						)
+					);
+					?>
 
 
-			<div class="xl:hidden min-w-full">
-				<div class="xl:relative min-w-full xl:flex xl:justify-between xl:items-center">
-					<div class="flex justify-between items-center min-w-full">
-					<?php
+<?php
 					wp_nav_menu(
 						array(
 							'theme_location'  => 'secondary',
 							'container'       => 'nav',
 							'container_id'    => 'secondary-menu',
-							'container_class' => 'hidden w-full md:mt-0 md:p-0 md:pl-16 md:block',
+							'container_class' => 'hidden w-full md:p-3 md:pl-16 md:block xl:hidden',
 							'menu_class'      => 'md:flex md:justify-center md:gap-4',
 							'li_class'        => 'md:mx-4 secondary-parent-li',
 							'a_class'         => 'ajax-link relative z-0 inline-block font-teko md:font-light md:text-4xl uppercase transition-all duration-150', 
@@ -51,8 +51,24 @@
 					);
 					?>
 
+<?php
+					wp_nav_menu(
+						array(
+							'theme_location'  => 'mobile',
+							'container'       => 'nav',
+							'container_id'    => 'mobile-menu',
+							'container_class' => 'mobile-menu z-1 top-0 absolute bg-black min-w-full',
+							'menu_class'      => 'menu-content mx-auto h-full flex flex-col justify-center w-1/3 gap-5',
+							'li_class'        => '', 
+							'a_class'         => 'font-teko text-xl', 
+							'submenu_class'   => 'ml-4', 
+							'fallback_cb'     => false,
+						)
+					);
+					?>
+
 								<!-- Wordpress classic logo and headlines -->
-					<div>
+
 							<?php if (has_custom_logo()) { ?>
 								<!-- <?php the_custom_logo(); ?>  -->
 							<?php } else { ?>
@@ -65,71 +81,23 @@
 								</p>
 
 							<?php } ?>
-						</div>
+
 
 
 
 								<!-- Hamburger Animation -->
-						<div class="p-4 z-10 xl:hidden">
+						<div class="p-4 z-10 absolute top-0 right-0 xl:hidden">
 						<a href="#" aria-label="Toggle navigation" id="mobile-menu-toggle">
 								<div id="toggleMenu" class="grid place-content-center w-10 h-10">
 								<div 
-								class="
-								w-10
-								h-1
-							  bg-gray-100 
-							    rounded-full
-								transition-all
-								duration-50
-								before:content-['']
-								before:absolute
-								before:w-10
-								before:h-1
-							    before:bg-gray-100 
-								before:rounded-full
-								before:-translate-y-4
-								before:transition-all
-								before:duration-150
-								after:content-['']
-								after:absolute
-								after:w-10
-								after:h-1
-								after:bg-gray-100 
-								after:rounded-full
-								after:translate-y-4
-								after:transition-all
-								after:duration-150
-								">
+								class="hamburger-icon">
 							</div>
 							</div>
 							</a>
 						</div>
-					</div>
-
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location'  => 'mobile',
-							'container'       => 'nav',
-							'container_id'    => 'mobile-menu',
-							'container_class' => 'mobile-menu top-0 absolute bg-black min-w-full',
-							'menu_class'      => 'menu-content mx-auto h-full flex flex-col justify-center w-1/3 gap-5',
-							'li_class'        => '', 
-							'a_class'         => 'z-1 font-teko text-xl', 
-							'submenu_class'   => 'ml-4', 
-							'fallback_cb'     => false,
-						)
-					);
-					?>
-
-			
-				</div>
-			</div>
 
 			
 
-
-			
 		</header>
 			<?php do_action('tailpress_content_start'); ?>
 
